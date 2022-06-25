179152245655295067ed4862fa9af59667fe75ae
anthonykimani <anthonykimani@student.moringaschool.com>
2022-06-25 22:11:37 +0300

    fetches images from API, tweaks minor css

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -18,3 +18,39 @@ const subtitle = document.querySelector('.gallery-text-subtitle');
 const like = document.querySelector('.like-button');
 const likeCount = document.querySelector('.like-count');
 const cartList = document.querySelector('.cart-list');
+
+const fetchProducts = ()=>{
+    fetch('https://fakestoreapi.com/products',{
+        method:'GET',
+        headers:{
+            'Access-Control-Allow-Origin': 'https://fakestoreapi.com/products'
+        }
+    })
+    .then((response)=>response.json())
+    .then((data)=>showProducts(data))
+    .catch((error)=>`error found:${error}`)
+}
+fetchProducts();
+
+const showProducts = (info)=>{
+    info.forEach((element)=>{
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        img.setAttribute('src',`${element.image}`);
+        img.setAttribute('class','product-image');
+        displayImage.setAttribute('src',`${element.image}`);
+        displayHeading.textContent = `${element.title}`;
+        displayParagraph.textContent = `${element.description}`;
+        displayPrice.textContent = `${element.price}`
+        img.addEventListener('click',(event)=>{
+            console.log('click');
+            displayImage.setAttribute('src',`${element.image}`)
+            displayHeading.textContent = `${element.title}`;
+            displayParagraph.textContent = `${element.description}`;
+            displayPrice.textContent = `$ ${element.price}`;
+            cartHandler(element);
+        })
+        div.appendChild(img);
+        itemDisplay.appendChild(div);
+    })
+}
